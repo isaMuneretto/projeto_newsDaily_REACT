700307d7a558774d7cff70a399dce921eaa52f86
isaMuneretto <isamuneretto@gmail.com>
2023-11-16 16:11:41 -0300

    estilo de compon. com styled compon. (cards)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,29 @@
-export function Card({news}){
-console.log(news)
+import { CardBody, CardContainer, CardFooter } from "./CardStyled"
+
+export function Card({ news }) {
+    console.log(news)
     return (
-        <section>{/*como aqui é html e os dados dentro das tags são JS, tem que interpolar com chaves */}
-            <h2>{news.title}</h2>
-            <p>{news.text}</p>
-            <img src={news.image} alt="Imagem" />
-            <i className="bi bi-hand-thumbs-up"></i>
-            <span>{news.likes}</span>
-            <i className="bi bi-chat"></i>
-            <span>{news.comments}</span>
-        </section>
+        <CardContainer>{/*tag section */}{/*como aqui é html e os dados dentro das tags são JS, tem que interpolar com chaves */}
+            <CardBody> {/*tag article */}
+                <div>
+                    <h2>{news.title}</h2>
+                    <p>{news.text}</p>
+                </div>
+                <img src={news.image} alt="Imagem" />
+            </CardBody>
+
+            <CardFooter>
+                <div>
+                    <i className="bi bi-hand-thumbs-up"></i>
+                    <span>{news.likes}</span>
+                </div>
+
+                <div>
+                    <i className="bi bi-chat"></i>
+                    <span>{news.comments}</span>
+                </div>
+
+            </CardFooter>
+        </CardContainer>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,18 @@
 import { Card } from "../../components/Card/Card.jsx";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { news } from '../../Datas.js' //buscar o conteudo mocado (ficticio) no Datas
+import { HomeBody } from "./HomeStyled.jsx";
 
-export default function Home (){
+export default function Home() {
     return ( //o return so pode retornar uma tag entao tem que envolver em uma section ou qlqr outra tag, div, etc no caso usei uma Fragment (tag sem nome) 
         <> {/*Fragment é uma tag vazia não precisa ter nome*/}
-            <Navbar/>
-            {news.map((item, index) => {
-                return <Card key={index} news={item}/>
-            })}
+            <Navbar />
+            <HomeBody>
+                {news.map((item, index) => {
+                    return <Card key={index} news={item} />
+                })}
+            </HomeBody>
+
         </>
     );
 }
