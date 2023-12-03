70e955a606664d32f97cb6d17b05c420529d7595
isaMuneretto <isamuneretto@gmail.com>
2023-12-03 18:28:25 -0300

    Pegando um valor de input com React Hook Form

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,53 @@
 // A navbar vai chamar a Home e a Search
+//no onsubmit que vai para o formulario, chama o handlesubmit que passa o parametro de onsearch que chama os dados(data)
+//esses dados precisam ser registrados (register)
+//no input ele vai pegar todos os dados (...) e mais o title
 
-import { Outlet } from "react-router-dom";
+
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import logo from "../../images/LogoND.png";
 import { Button, ImageLogo, InputSpace, Nav } from "./NavbarStyled";
+import { useForm } from "react-hook-form";
 //import "./Navbar.css";
 
 export function Navbar() {
+    const { register, handleSubmit, reset } = useForm(); //parametros desestruturados do HookForm(usado para formularios)
+
+    //se usa o useNavigate pq não vou criar um link no html do return, eu vou mudar de rota pelo javascript
+    const navigate = useNavigate(); //reacte-router-dom
+
+    function onSearch(data) {//o que eu quero que aconteça qnd eu fizer o onSubmit é entrar no onSearch e receber os dados(data)
+        const { title } = (data);
+        //navega na pagina search e vai pegar o title do parametro e vai mostrar
+        navigate(`/search/${title}`);
+        reset();
+    }
 
     return (
         <>
             <Nav> {/* essas tags são componentes que chamam o arquivo NavbarStyled*/}
-                <InputSpace className="input-search-space">
-                    <i className="bi bi-search"> {/*tag de icone*/}</i>
-                    <input type="text" placeholder="Pesquise por um título " />{/*as tags i e input não viraram componente CSS pq lá no styled foi feito herança */}
+                <form onSubmit={handleSubmit(onSearch)}>
+                    <InputSpace className="input-search-space">
+                        <button type="submit">
+                            <i className="bi bi-search"> {/*tag de icone*/}</i>
+                        </button>
 
-                </InputSpace>
+                        <input {...register("title")}
+                            type="text"
+                            placeholder="Pesquise por um título "
+                        />{/*as tags i e input não viraram componente CSS pq lá no styled foi feito herança */}
+                    </InputSpace>
+                </form>
 
-                <ImageLogo src={logo} alt="Logo " />
+                <Link to="/"> {/*esse link é recurso do router-dom e substitui o href para navegação interna */}
+                    <ImageLogo src={logo} alt="Logo " />
+                </Link>
 
                 <Button>Login</Button> {/*ao inves de ser um componente HTML, agora é componente CSS*/}
 
                 <Button>Registre-se</Button>
             </Nav>
-            <Outlet/> {/*a outlet é uma rota que fica embaixo da navbar portanto ela não pode ser colocada em outro lugar*/}
+            <Outlet /> {/*a outlet é uma rota que fica embaixo da navbar portanto ela não pode ser colocada em outro lugar*/}
         </>
     )
 }
--- src/components/Navbar/NavbarStyled.jsx
+++ src/components/Navbar/NavbarStyled.jsx
@@ -31,7 +31,7 @@ export const InputSpace = styled.div`
   align-items: center;
   margin-left: 10px;
 
-  i {
+  button {
     position: absolute;
     top: 1;
     right: 0.2rem;
@@ -41,6 +41,13 @@ export const InputSpace = styled.div`
     color: #757575;
     border-radius: 0.3rem;
     padding: 0.5rem;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+
+  button:hover{
+    background-color: #757575;
+    color: #f5f5f5;
   }
 
   input {
